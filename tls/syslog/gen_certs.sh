3b05207ee6615db83a15dbb5220605f4b148e0c2
Andrew Hromis <ahromis@gmail.com>
2016-07-13 11:13:37 -0400

    working with localhost

--- tls/syslog/gen_certs.sh
+++ tls/syslog/gen_certs.sh
@@ -9,7 +9,7 @@ if [ ! ${ca_pass} ]; then
 fi
 
 if [ ! ${cn} ]; then
-    read -p "Enter in your certificate CN: " cn
+    read -p "Enter in your certificate CN (Common Name): " cn
 fi
 
 echo 'Creating CA (ca-key.pem, ca.pem)'
@@ -19,7 +19,7 @@ openssl req -new -passin pass:${ca_pass} -x509 -days 3650  -subj '/CN=DemoCA/C=U
 echo 'Creating server certificates (key.pem, cert.pem)'
 openssl genrsa -passout pass:${ca_pass} -out pkcs5-key.pem 4096
 openssl req  -passin pass:${ca_pass} -subj "/CN=${cn}" -sha256 -new -key pkcs5-key.pem -out server.csr
-echo "subjectAltName = IP:${cn}" > extfile.cnf
+echo "subjectAltName = DNS:${cn}" > extfile.cnf
 openssl x509 -passin pass:${ca_pass} -req -days 3650 -sha256 -in server.csr -CA ca.pem -CAkey ca-key.pem -CAcreateserial -out cert.pem -extfile extfile.cnf
 openssl pkcs8 -in pkcs5-key.pem -topk8 -nocrypt -out pkcs8-key.pem
 mv pkcs8-key.pem key.pem
