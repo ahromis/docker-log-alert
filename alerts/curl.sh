57d979f4511e0dc9e351f0442b0f5e19416bd3e8
Andrew Hromis <ahromis@gmail.com>
2016-07-15 12:52:03 -0400

    finalized slack integration

---
+++ alerts/curl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Health check registered in the Consul API"
+echo "======================================="
+curl -s http://localhost:8500/v1/health/service/nginx-demo | jq .
+echo ""
+
+echo "Add in Slack integration with KV values"
+echo "======================================="
+echo 'curl -s -X PUT -H 'Content-Type: application/json' -d "true" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/enabled'
+curl -s -X PUT -H 'Content-Type: application/json' -d "true" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/enabled; echo ""
+echo 'curl -s -X PUT -H 'Content-Type: application/json' -d "https://hooks.slack.com/services/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" http://localhost:8500/v1    /kv/consul-alerts/config/notifiers/slack/url'
+curl -s -X PUT -H 'Content-Type: application/json' -d "https://hooks.slack.com/services/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" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/url; echo ""
+echo 'curl -s -X PUT -H 'Content-Type: application/json' -d "#general" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/channel'
+curl -s -X PUT -H 'Content-Type: application/json' -d "#general" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/channel; echo ""
+echo ""
+
+echo "Decrease notification send time"
+echo "======================================="
+echo 'curl -s -X PUT -H 'Content-Type: application/json' -d "30" http://localhost:8500/v1/kv/consul-alerts/config/checks/change-threshold; echo ""'
+curl -s -X PUT -H 'Content-Type: application/json' -d "30" http://localhost:8500/v1/kv/consul-alerts/config/checks/change-threshold; echo ""
+echo ""
+
+echo "Values in Consul KV store"
+echo "======================================="
+curl -s http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/?recurse | jq .
+
+echo ""
+curl -s http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/enabled?raw; echo ""
+curl -s http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/url?raw; echo ""
+curl -s http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/channel?raw; echo ""
+
