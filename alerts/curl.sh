509352839d36bdac58725e7e6681c317430e0846
Andrew Hromis <ahromis@gmail.com>
2016-07-17 21:03:39 -0700

    decreased flapping time for demo

--- alerts/curl.sh
+++ alerts/curl.sh
@@ -8,18 +8,18 @@ echo ""
 
 echo "Add in Slack integration with KV values"
 echo "======================================="
-echo 'curl -s -X PUT -H 'Content-Type: application/json' -d "true" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/enabled'
-curl -s -X PUT -H 'Content-Type: application/json' -d "true" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/enabled; echo ""
-echo 'curl -s -X PUT -H 'Content-Type: application/json' -d "https://hooks.slack.com/services/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" http://localhost:8500/v1    /kv/consul-alerts/config/notifiers/slack/url'
-curl -s -X PUT -H 'Content-Type: application/json' -d "https://hooks.slack.com/services/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" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/url; echo ""
-echo 'curl -s -X PUT -H 'Content-Type: application/json' -d "#general" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/channel'
-curl -s -X PUT -H 'Content-Type: application/json' -d "#general" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/channel; echo ""
+echo 'curl -s -X PUT -d "true" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/enabled'
+curl -s -X PUT -d "true" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/enabled; echo ""
+echo 'curl -s -X PUT -d "https://hooks.slack.com/services/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" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/url'
+curl -s -X PUT -d "https://hooks.slack.com/services/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" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/url; echo ""
+echo 'curl -s -X PUT -d "#general" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/channel'
+curl -s -X PUT -d "#general" http://localhost:8500/v1/kv/consul-alerts/config/notifiers/slack/channel; echo ""
 echo ""
 
 echo "Decrease notification send time"
 echo "======================================="
-echo 'curl -s -X PUT -H 'Content-Type: application/json' -d "30" http://localhost:8500/v1/kv/consul-alerts/config/checks/change-threshold'
-curl -s -X PUT -H 'Content-Type: application/json' -d "30" http://localhost:8500/v1/kv/consul-alerts/config/checks/change-threshold; echo ""
+echo 'curl -s -X PUT -d "20" http://localhost:8500/v1/kv/consul-alerts/config/checks/change-threshold'
+curl -s -X PUT -d "20" http://localhost:8500/v1/kv/consul-alerts/config/checks/change-threshold; echo ""
 echo ""
 
 echo "Values in Consul KV store"
